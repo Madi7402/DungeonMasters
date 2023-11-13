1d497b15a0d8b6bf307dd45be61e7275686d7fc1
Jonathan Abrams <jabrams9@uw.edu>
2023-11-12 21:42:27 -0800

    Example save/load concept for characters

--- src/view/Tui.java
+++ src/view/Tui.java
@@ -3,13 +3,43 @@ import model.*;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.*;
 
 import static controller.PropertyChangeEnableFight.DEATH;
 
 public class Tui implements PropertyChangeListener {
-    public static void main(String[]theArgs) {
+    public static void main(String[]theArgs) throws IOException, ClassNotFoundException {
         Tui tui = new Tui();
-        tui.fightSim();
+//        tui.fightSim();
+        tui.saveLoadConcept();
+    }
+
+    private void saveLoadConcept() throws IOException, ClassNotFoundException {
+        Hero hero = new Thief("Billy");
+        System.out.println("Original hero: " + hero);
+        MonsterFactory mf = new MonsterFactory();
+        Monster ogre = mf.createMonster(MonsterType.OGRE);
+        ogre.addPropertyChangeListener(this);
+        hero.addPropertyChangeListener(this);
+        while (hero.getMyHealthPoints() >= 75) {
+            ogre.attack(hero);
+        }
+        System.out.println("Original hero: " + hero);
+
+        // Save
+        FileOutputStream outputFile = new FileOutputStream("savedhero.txt");
+        ObjectOutputStream outputObj = new ObjectOutputStream(outputFile);
+        outputObj.writeObject(hero);
+        outputObj.flush();
+        outputObj.close();
+
+        // Load
+        FileInputStream inputFile = new FileInputStream("savedhero.txt");
+        ObjectInputStream inputObj = new ObjectInputStream(inputFile);
+        Hero newhero = (Hero) inputObj.readObject();
+        inputObj.close();
+
+        System.out.println("New Hero: " + newhero.toString());
     }
 
     public void fightSim() {
