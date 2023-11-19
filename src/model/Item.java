a88280e5ef9f2bb9967049c4fac5b3f54ed45520
Jonathan Abrams <jabrams9@uw.edu>
2023-11-18 17:55:20 -0800

    Implement Item class to get Item name/description from DB
    In the future we may choose to also get various stats modifiers and other parameters explaining the use of the items.
    
    For instance, an item with positive health_points such as a potion could increase the health points of the character using it, and one with negative could be considered an offensive item which could be "used" on another character such as a monster.
    
    For the stat_multiplier field of the item Table we may be able to implement a system where a character holding an item gains a certain stat boost (for instance after collecting a pillar)

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,8 +1,64 @@
 package model;
 
+import org.sqlite.SQLiteDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * An item within the dungeon that can be used by the player. Uses SQLite.
  * @author Jonathan Abrams, Madison Pope, Martha Emerson
  */
 public class Item {
+    /**
+     * The Name (identifier) of the Item.
+     */
+    private String myName;
+    /**
+     * Text description of the Item.
+     */
+    private String myDesc;
+
+    /**
+     * Create an Item given an ItemType.
+     * @param theItem the ItemType we are creating.
+     */
+    public Item(final ItemType theItem) {
+        final SQLiteDataSource ds = new SQLiteDataSource(); // From example code
+        ds.setUrl("jdbc:sqlite:database.sqlite.db");
+        final String query = "SELECT * FROM item where name == '" + theItem.toString().toLowerCase() + "'"; // TODO -JA: SQLi potential
+
+        try (Connection conn = ds.getConnection();
+             Statement stmt = conn.createStatement()) {
+            final ResultSet rs = stmt.executeQuery(query);
+            myName = rs.getString("friendly_name");
+            myDesc = rs.getString("description"); // TODO -JA: Expose other fields
+        } catch (final SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Get the friendly name of the Item.
+     * @return the friendly name (e.g. "Pillar of Abstraction")
+     */
+    public String getName() {
+        return myName;
+    }
+
+    /**
+     * Get the Item's description.
+     * @return the description (e.g. "Stay Hidden.")
+     */
+    public String getDescription() {
+        return myDesc;
+    }
+
+    @Override
+    public String toString() {
+        return myName + ": \"" + myDesc + "\"";
+    }
 }
