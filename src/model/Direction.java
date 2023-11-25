0bc2393112a25575b26b8ee3749fb5b0a5e8feba
m-a-r-t-s <martsUW@proton.me>
2023-11-25 11:33:10 -0800

    Created SoundManager class, added documentation to enums, record and new class.

--- src/model/Direction.java
+++ src/model/Direction.java
@@ -1,7 +1,10 @@
 package model;
 
 /**
- * An Enum of Directions (N S E W).
+ * Represents cardinal directions (North, South, East, West).
+ * Each direction has associated x and y offsets indicating movement on a 2D grid.
+ * The x and y offsets allow for easy navigation on a 2D grid based on the direction.
+ *
  * @author Jonathan Abrams, Martha Emerson, Madison Pope
  */
 public enum Direction {
@@ -10,21 +13,42 @@ public enum Direction {
     EAST(1, 0),   // Move east (right)
     WEST(-1, 0);  // Move west (left)
 
+    /**
+     * The x offset indicating movement in the horizontal direction.
+     */
     private final int xOffset;
+
+    /**
+     * The y offset indicating movement in the vertical direction.
+     */
     private final int yOffset;
 
-    // Direction (determined by x and y offsets)
+    /**
+     * Constructs a direction with the specified x and y offsets.
+     *
+     * @param xOffset The x offset indicating movement in the horizontal direction.
+     * @param yOffset The y offset indicating movement in the vertical direction.
+     */
     Direction(int xOffset, int yOffset) {
         this.xOffset = xOffset;
         this.yOffset = yOffset;
     }
 
+    /**
+     * Gets the x offset indicating movement in the horizontal direction.
+     *
+     * @return The x offset.
+     */
     public int getXOffset() {
         return xOffset;
     }
 
+    /**
+     * Gets the y offset indicating movement in the vertical direction.
+     *
+     * @return The y offset.
+     */
     public int getYOffset() {
         return yOffset;
     }
 }
-
--- src/model/Portal.java
+++ src/model/Portal.java
@@ -2,6 +2,10 @@ package model;
 
 /**
  * An Enum of Portal types.
+ * Portals can be of three types: NONE, ENTRANCE, and EXIT.
+ * NONE represents a room that is not a special portal, ENTRANCE represents the entrance portal,
+ * and EXIT represents the exit portal.
+ *
  * @author Jonathan Abrams, Martha Emerson, Madison Pope
  */
 public enum Portal {
