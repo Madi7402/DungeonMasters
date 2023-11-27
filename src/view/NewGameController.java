b611a8a75b220590f468cb894c8205ea0fdb88a7
Jonathan A. <jabrams9@uw.edu>
2023-11-27 13:42:13 -0800

    Create SQLite Object for making queries
    Now we have a centralized SQLite objects to make queries with a more condensed "Try with resources" setup.
    
    Now we don't have to instance various SQL related objects in unrelated classes  and can instead query the SQLiteDB as such:
    
        try (SQLite db = new SQLite(query))
    
     As the SQLite class implements AutoClosable the database will be properly closed at the end of the try block.

--- src/view/NewGameController.java
+++ src/view/NewGameController.java
@@ -3,6 +3,7 @@ package view;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import org.sqlite.SQLiteDataSource;
+import res.SQLite;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,24 +13,14 @@ import java.sql.Statement;
 public class NewGameController {
 
     @FXML
-    private ComboBox<String> herotype;
+    private ComboBox<String> myHeroTypeCB;
 
-    @FXML
     public void initialize() {
-        // Initialization logic, if needed
-        ComboBox<String> cb = herotype;
-
-        final SQLiteDataSource ds = new SQLiteDataSource(); // From example code
-        ds.setUrl("jdbc:sqlite:database.sqlite.db");
-
-        final String query = "SELECT friendly_name FROM character where isHero == 1";
-
-        try (Connection conn = ds.getConnection();
-             Statement stmt = conn.createStatement()) {
-
-            final ResultSet rs = stmt.executeQuery(query);
+        final String query = "SELECT friendly_name FROM character where isHero = 1";
+        try (SQLite db = new SQLite(query)) {
+            ResultSet rs = db.getMyResults();
             while (rs.next()) {
-                cb.getItems().add(rs.getString("friendly_name"));
+                myHeroTypeCB.getItems().add(rs.getString("friendly_name"));
             }
         } catch (final SQLException e) {
             e.printStackTrace();
