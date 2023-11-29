4d044f86bce6cdebb7903c14a1eb79a16affad52
Jonathan Abrams <jabrams9@uw.edu>
2023-11-28 18:32:29 -0800

    In NewGame, Show an image for each Hero Type

--- src/view/NewGameController.java
+++ src/view/NewGameController.java
@@ -1,10 +1,11 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import model.DungeonCharacter;
 import res.SQLite;
@@ -15,7 +16,7 @@ import java.sql.SQLException;
 public class NewGameController {
 
     @FXML
-    private ComboBox<String> myHeroTypeCB;
+    private ChoiceBox<String> myHeroTypeCB;
 
     @FXML
     private Slider myMapWidthSlider;
@@ -38,26 +39,62 @@ public class NewGameController {
     @FXML
     private Button myPlayButton;
 
+    @FXML
+    private ImageView myHeroImageView;
+
     public void initialize() {
-        final String query = "SELECT friendly_name FROM character where isHero = 1";
+        // Select Hero Type
+        myHeroTypeCB.setOnAction(actionEvent -> {
+            System.out.println("selected action " + actionEvent.toString());
+            String query = "SELECT img FROM character where friendly_name = '" + myHeroTypeCB.getValue() + "'";
+            try (SQLite db = new SQLite(query)) {
+                System.out.println("Querying: " + query);
+                ResultSet rs = db.getMyResults();
+                if (rs.next()) {
+                    String imageName = rs.getString("img");
+                    if (imageName != null) {
+                        try {
+                            myHeroImageView.setImage(new Image("img/" + imageName));
+                        } catch (IllegalArgumentException iae) {
+                            System.err.print("Received Image path (" + imageName + ") from Database, However: ");
+                            System.err.println(iae.getMessage());
+                        }
+                    } else {
+                        myHeroImageView.setImage(null); // TODO -JA: Set to a default image
+                        System.err.println(myHeroTypeCB.getValue() + " has no image!");
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        });
+
+        // Populate HeroType Choice Box
+        String query = "SELECT friendly_name FROM character where isHero = 1";
         try (SQLite db = new SQLite(query)) {
             ResultSet rs = db.getMyResults();
             while (rs.next()) {
                 myHeroTypeCB.getItems().add(rs.getString("friendly_name"));
             }
+
+            myHeroTypeCB.setValue(myHeroTypeCB.getItems().get(0));
         } catch (final SQLException e) {
             e.printStackTrace();
             System.exit(1);
         }
 
+        // Write slider value text
         myMapWidthSlider.valueProperty().addListener(
                 (observable, oldVal, newVal) -> myWidthSliderValue.setText(newVal.intValue()+"")
         );
 
+        // Write slider value text
         myMapHeightSlider.valueProperty().addListener(
                 (o, oldVal, newVal) -> myHeightSliderValue.setText(newVal.intValue()+"")
         );
 
+        // Display Hero name under Image and clamp to MAX_NAME_LENGTH
         myHeroNameTextField.setOnKeyTyped(keyEvent -> {
             TextField tf = myHeroNameTextField;
             if (tf.getText().length() > DungeonCharacter.MAX_NAME_LENGTH) { // Clamp max HERO length
@@ -66,5 +103,6 @@ public class NewGameController {
             }
             myHeroNameDisplayText.setText(tf.getText());
         });
+
     }
 }
