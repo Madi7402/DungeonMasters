034d5525a0ca0d66625237c25ed5ee04b32e9b7d
m-a-r-t-s <martsUW@proton.me>
2023-12-15 14:50:52 -0800

    Update AbstractRoomFactory Updated Javadocs for the AbstractRoomFactory class

--- src/model/AbstractRoomFactory.java
+++ src/model/AbstractRoomFactory.java
@@ -3,8 +3,12 @@ package model;
 /**
  * Abstract factory class for creating rooms.
  * Concrete subclasses should implement the method to create a room with specific characteristics.
+ * This abstract class defines the contract for creating rooms, allowing for different room creation
+ * strategies in subclasses.
+ *
+ * @author Jonathan Abrams, Martha Emerson, Madison Pope
  */
-public abstract class AbstractRoomFactory { // TODO - fyi this does not need a test
+public abstract class AbstractRoomFactory { // TODO - fyi this does not need unit tests
 
     /**
      * Creates a room with specific characteristics based on the given theCoordinates.
