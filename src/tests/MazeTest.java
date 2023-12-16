4a6be0147b4c9457d4392e235e582ef0f0506d1b
m-a-r-t-s <martsUW@proton.me>
2023-12-15 17:16:10 -0800

    Add unit tests for Maze Added unit tests to MazeTest class

--- src/tests/MazeTest.java
+++ src/tests/MazeTest.java
@@ -1,14 +1,15 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import model.AbstractRoomFactory;
-import model.Maze;
-import model.RandomRoomFactory;
+import model.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 
 public class MazeTest {
 
@@ -33,4 +34,92 @@ public class MazeTest {
     // ^^^ maybe make it assert not null or not empty
     // TODO - make a procedural room factory
     // ^^ do the tests with toString = ... (must print something exactly)
+
+    @Test
+    void testMazeInitialization() {
+        Maze maze = new Maze(myRandomRoomFactory, 5, 5);
+
+        assertNotNull(maze);
+        assertEquals(5, maze.getWidth());
+        assertEquals(5, maze.getHeight());
+        assertEquals(4, Maze.LEVELS);
+    }
+
+    @Test
+    void testMazeTraversal() {
+        Maze maze = new Maze(myRandomRoomFactory, 5, 5);
+
+        ValidMazeCriteria criteria = new ValidMazeCriteria();
+        Coordinates entrance = maze.getStartingRoom().getCoordinate();
+        boolean traversalResult = maze.TraverseTo(entrance, criteria);
+
+        assertTrue(traversalResult);
+        assertTrue(criteria.isValid());
+    }
+
+    @Test
+    void testGetStartingRoom() {
+        Maze maze = new Maze(myRandomRoomFactory, 5, 5);
+
+        Room startingRoom = maze.getStartingRoom();
+
+        assertNotNull(startingRoom);
+        assertEquals(Portal.ENTRANCE, startingRoom.getPortal());
+    }
+
+    @Test
+    void testGetRoom() {
+        Maze maze = new Maze(myRandomRoomFactory, 5, 5);
+
+        Coordinates coordinates = new Coordinates(0, 0, 0);
+        Room room = maze.getRoom(coordinates);
+
+        assertNotNull(room);
+        assertEquals(coordinates, room.getCoordinate());
+    }
+
+    @Test
+    void testGetRoomWithDirection() {
+        Maze maze = new Maze(myRandomRoomFactory, 5, 5);
+
+        Coordinates coordinates = new Coordinates(0, 0, 0);
+        Room room = maze.getRoom(coordinates, Direction.NORTH);
+
+        assertNotNull(room);
+        assertEquals(coordinates.generate(Direction.NORTH), room.getCoordinate());
+    }
+
+    @Test
+    void testSetSurroundingRoomsVisible() {
+        Maze maze = new Maze(myRandomRoomFactory, 5, 5);
+
+        Coordinates center = new Coordinates(0, 2, 2);
+        int radius = 1;
+
+        maze.setSurroundingRoomsVisible(center, radius);
+
+        List<Coordinates> coordinatesList = getSurroundingCoordinates(center, radius);
+        for (Coordinates coordinates : coordinatesList) {
+            Room room = maze.getRoom(coordinates);
+            assertNotNull(room);
+            assertTrue(room.isVisited());
+        }
+    }
+
+    private List<Coordinates> getSurroundingCoordinates(Coordinates center, int radius) {
+        List<Coordinates> surroundingCoordinates = new ArrayList<>();
+
+        if (radius >= 0) {
+            for (int xOffset = -radius; xOffset <= radius; xOffset++) {
+                for (int yOffset = -radius; yOffset <= radius; yOffset++) {
+                    Coordinates surroundingCoord = center.generate(xOffset, yOffset);
+                    surroundingCoordinates.add(surroundingCoord);
+                }
+            }
+        } else {
+            surroundingCoordinates.add(center);
+        }
+
+        return surroundingCoordinates;
+    }
 }
