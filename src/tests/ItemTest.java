3182f41e2a3bc35f917f6e872abf15412321b1ab
Jonathan Abrams <jabrams9@uw.edu>
2023-12-15 17:01:42 -0800

    Additional Item testing

--- src/tests/ItemTest.java
+++ src/tests/ItemTest.java
@@ -19,8 +19,21 @@ public class ItemTest {
     }
 
     @Test
-    public void getNameDescription() {
+    public void getNameDescriptionTest() {
         Item pillar = new Item(ItemType.PILLAR_ABSTRACTION);
         assertEquals("Stay Hidden.", pillar.getDescription());
+    }
+
+    @Test
+    public void getMyStatMultiplierTest() {
+        Item pillar = new Item(ItemType.PILLAR_ABSTRACTION);
+        assertEquals(1.15, pillar.getMyStatMultiplier());
+    }
+
+    @Test
+    public void getMyIconTest() {
+        Item pillar = new Item(ItemType.PILLAR_ABSTRACTION);
+        assertNull(pillar.getMyIcon());
+        // if we were to add icons, we would also check for a non-null case
     }
 }
